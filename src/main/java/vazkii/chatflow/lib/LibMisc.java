62b9938111b6d02231893e6c517b5edd88f24c71
Vasco Lavos <vazkii@hotmail.com>
2017-01-02 23:15:20 +0000

    1.11

--- src/main/java/vazkii/chatflow/lib/LibMisc.java
+++ src/main/java/vazkii/chatflow/lib/LibMisc.java
@@ -2,7 +2,7 @@ package vazkii.chatflow.lib;
 
 public class LibMisc {
 
-	public static final String MOD_ID = "ChatFlow";
+	public static final String MOD_ID = "chatflow";
 	public static final String MOD_NAME = "ChatFlow";
 	public static final String MOD_VERSION = "ANT:VERSION";
 
